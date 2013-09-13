b1abb7f0e3a9101750f29eb39a3e5c0f627dbd9e
azm-gh <github@centrum.cz>
2013-09-13 20:52:40 +0200

    Initial Addition class

---
+++ header.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class testing {
+public: 
+	int addition(int a, int b);
+	
+};
+
+int testing::addition(int a, int b)
+	{
+		int r = a + b;
+		return(r);
+	}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "header.h"
+
+int main()
+{	
+	testing test1;
+	int a =test1.addition(5,6);
+	std::cout << a << std::endl;
+	return 0;
+}
