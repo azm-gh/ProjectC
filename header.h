6a427fbeed62964325138817e645f040b75a1d16
azm-gh <github@centrum.cz>
2013-09-14 11:38:40 +0200

    Edited master

--- header.h
+++ header.h
@@ -1,8 +1,12 @@
 #include <iostream>
 
 class testing {
+
 public: 
 	int addition(int a, int b);
+	void bubblesort(int *array, int size);
+	void printvalues(int *array, int size);
+	
 	
 };
 
@@ -11,3 +15,25 @@ int testing::addition(int a, int b)
 		int r = a + b;
 		return(r);
 	}
+
+void testing::bubblesort(int *array, int size)
+{
+	for(int i = 0; i<size-1; i++)
+	{
+		for(int j = 0; j<size-i-1; j++)
+		{
+			if(array[j]>array[j+1]){
+				int val = array[j];
+				array[j] = array[j+1];
+				array[j+1] = val;
+			}
+		}
+	}
+}
+
+void testing::printvalues(int *array, int size)
+{
+	for(int i=0; i< size; i++)
+		std::cout << array[i] << " ";
+}
+
--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include "header.h"
 
+/*void printvalue(int array)
+{
+	for(int i=0; i< size; i++)
+		std::cout << i << " ";
+}*/
+
+
 int main()
 {	
 	testing test1;
 	int a =test1.addition(5,6);
 	std::cout << a << std::endl;
+	
+	int x[] ={45,4,23,2,16};
+	test1.bubblesort(x,5);
+	test1.printvalues(x,5);
+
 	return 0;
 }
+
+	
+
+	/*for(int i=0; i< size; i++)
+		std::cout << i << " ";
+}
+	printvalue(x,5);
+	return 0;
+}*/
+
+
